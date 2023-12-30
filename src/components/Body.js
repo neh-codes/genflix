d51ab4bfc60aa17e9f1801f386f84a294e744ad6
neha <neh-codes@gmail.com>
2023-12-30 13:25:10 +0530

    Fix: Authetication sign out

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,17 +1,14 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import Login from './Login';
 import Browse from './Browse';
-import {useDispatch} from "react-redux";
 import {RouterProvider} from "react-router-dom";
 import { createBrowserRouter } from 'react-router-dom';
-import { onAuthStateChanged } from 'firebase/auth';
-import {auth} from '../utils/firebase';
-import {addUser, removeUser} from '../utils/userSlice';
+
 
 
 const Body = () => {
-  const dispatch = useDispatch();
-  
+
+
   const appRouter = createBrowserRouter([
     {
       path: "/",
@@ -24,29 +21,12 @@ const Body = () => {
   ]);
 
 
-  useEffect(()=>{
-    onAuthStateChanged(auth, (user) => {
-      if (user) {
-        // User is signed in, see docs for a list of available properties
-        // https://firebase.google.com/docs/reference/js/auth.user
-        const {uid, email, displayName, photoURL} = user;
-        dispatch(addUser({uid:uid, email:email, displayName: displayName, photoURL:photoURL}))
-        
 
-
-      } else {
-       dispatch(removeUser());
-      
-      }
-    });
-  }, [])
 
 
   return (
     <div>
       <RouterProvider router={appRouter}/>
-       
-      
     </div>
   )
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -2,11 +2,14 @@ import React from 'react';
 import { auth } from '../utils/firebase';
 import { useNavigate } from 'react-router-dom';
 import { signOut } from 'firebase/auth';
-import { useSelector } from 'react-redux';
-
+import { useSelector, useDispatch } from 'react-redux';
+import {onAuthStateChanged} from 'firebase/auth';
+import { useEffect } from 'react';
+import { addUser, removeUser } from '../utils/userSlice';
 
 const Header = () => {
   const navigate = useNavigate();
+  const dispatch = useDispatch ();
   const user = useSelector(store => store.user);
 
   const handleSignOut = () =>{
@@ -18,6 +21,30 @@ const Header = () => {
       navigate("/error");
     });
   }
+
+  useEffect(()=>{
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        // User is signed in, see docs for a list of available properties
+        // https://firebase.google.com/docs/reference/js/auth.user
+        const {uid, email, displayName, photoURL} = user;
+        dispatch(addUser({uid:uid, 
+          email:email, 
+          displayName: displayName, 
+          photoURL:photoURL
+        })
+        );
+        navigate("/browse");
+
+
+      } else {
+       dispatch(removeUser());
+       navigate("/");
+      }
+    });
+  }, []);
+
+
   return (
     <div className='bg-gradient-to-b from-black p-2 flex justify-between'>
       <img alt='logo' className='w-32 md:w-44' src='https://cdn.cookielaw.org/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png'/>
